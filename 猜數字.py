af99900804691fc99f758ec45bf70eb82ecf0f27
KUCHIATUAN <133851329+KUCHIATUAN@users.noreply.github.com>
2023-05-17 16:27:48 +0800

    Add files via upload

---
+++ 猜數字.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+ans=random.randint(1, 100)
+
+while True:
+    nn=int(input("Guess:"))
+    if nn==0:
+        break
+    elif nn>ans:
+        print("TOO BIG!!!")
+    elif nn<ans:
+        print("TOO SMALL!!!")
+    else:
+        print("答對了")
+        time.sleep(3)
+        ans=random.randint(1, 100)
+       
